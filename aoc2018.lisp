526890ead98be6cf0a00a4cc8a31db7cb5e1ca79
BuildTools <unconfigured@null.spigotmc.org>
2018-12-03 23:39:42 +0100

    added read-line added d1b - solution.

--- aoc2018.lisp
+++ aoc2018.lisp
@@ -5,11 +5,36 @@
 
 ;;;; Day 1
 
+
+;;;Input - file #p"~/quicklisp/local-projects/aoc2018/aoc2018_1_input.txt"
+
+
+(defun read-file (file &optional (fn NIL))
+  "Reads a line of input and applys, if given, fn to it. The result is a list of fn'ed input-lines."
+  (with-open-file (in (pathname file))
+    (loop for line = (read-line in NIL NIL)
+       while line
+       collect (if fn
+		   (funcall fn line)
+		   line))))
+		   
+
 ;;; A
 
 (defun d1a (file)
-  ;; #p"~/quicklisp/local-projects/aoc2018/aoc2018_1_a_input.txt"
-  (apply #'+ (with-open-file (in file)
-	       (loop for line = (read-line in NIL NIL)
-		  while line
-		  collect (parse-integer line)))))
+  (apply #'+ (read-file file)))
+
+;;; B
+
+(defun d1b (input)
+  (let ((cache (make-hash-table))
+	(sum 0))
+    (setf (gethash 0 cache) 't)
+    (loop
+       do (loop for number in input
+	     do
+	       (setf sum (+ sum number))
+	       (if (gethash sum cache)
+		   (return-from d1b sum))
+	       (setf (gethash sum cache) t)))))
+
