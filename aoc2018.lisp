73e87cfa1f4522c528ced97eecb845f89bf588ac
BuildTools <unconfigured@null.spigotmc.org>
2018-12-04 01:06:21 +0100

    Finished d2a and d2b.

--- aoc2018.lisp
+++ aoc2018.lisp
@@ -38,3 +38,45 @@
 		   (return-from d1b sum))
 	       (setf (gethash sum cache) t)))))
 
+;;;; Day 2
+
+;;; A
+(defun find-multiples (input)
+  (let ((counter (make-hash-table))
+	(result NIL))
+    (loop for c across input do
+	 (incf (gethash c counter 0)))
+    (setf result (loop for v being the hash-values in counter collect v))
+    (values (> (count-if #'(lambda (x) (= x 2)) result) 0)
+	    (> (count-if #'(lambda (x) (= x 3)) result) 0))))
+
+(defun d2a (input)
+  (let ((doubles 0)
+	(triples 0))
+    (loop :for id :in input
+       :do
+       (multiple-value-bind (double triple) (find-multiples id)
+	 (if double (incf doubles))
+	 (if triple (incf triples))))
+    (* doubles triples)))
+
+;;; B
+
+(defun diff-id (id1 id2)
+  (let ((result (make-array 1 :element-type 'character :adjustable t :fill-pointer 0)))
+    (loop :for index :from 0 :below (length id1)
+       :do
+	 (when (char= (aref id1 index) (aref id2 index))
+	    (vector-push-extend (aref id1 index) result)))
+    result))
+
+(defun d2b (input)
+  (if (null input) NIL)
+  (let* ((first-id (car input))
+	 (len (length first-id)))
+    (loop :for id :in (cdr input)
+       :do
+	 (let ((diff (diff-id first-id id)))
+	   (if (= (- len (length diff)) 1)
+	       (return-from d2b diff))))
+    (d2b (cdr input))))
